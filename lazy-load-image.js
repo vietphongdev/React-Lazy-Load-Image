e3957dbc460615b24ff789641079c3301fa7ffcd
vietphong <vietphong@vietphongs-MacBook-Pro.local>
2023-05-03 16:34:37 +0700

    first commit

---
+++ lazy-load-image.js
@@ -0,0 +1,58 @@
+const LazyLoadImage = ({
+  alt,
+  src,
+  className,
+  loadInitially = false,
+  observerOptions = { root: null, rootMargin: "200px 0px" },
+  ...props
+}) => {
+  const observerRef = React.useRef(null);
+  const imgRef = React.useRef(null);
+  const [isLoaded, setIsLoaded] = React.useState(loadInitially);
+
+  const observerCallback = React.useCallback(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        observerRef.current.disconnect();
+        setIsLoaded(true);
+      }
+    },
+    [observerRef]
+  );
+
+  React.useEffect(() => {
+    if (loadInitially) return;
+
+    if ("loading" in HTMLImageElement.prototype) {
+      setIsLoaded(true);
+      return;
+    }
+
+    observerRef.current = new IntersectionObserver(
+      observerCallback,
+      observerOptions
+    );
+    observerRef.current.observe(imgRef.current);
+    return () => {
+      observerRef.current.disconnect();
+    };
+  }, []);
+
+  return (
+    <img
+      alt={alt}
+      src={isLoaded ? src : ""}
+      ref={imgRef}
+      className={className}
+      loading={loadInitially ? undefined : "lazy"}
+      {...props}
+    />
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <LazyLoadImage
+    src="https://picsum.photos/id/1080/600/600"
+    alt="Strawberries"
+  />
+);
